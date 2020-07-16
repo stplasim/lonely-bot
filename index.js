e4f1fe109f75b8f2a8d1abe311161040321a0c8a
Simon Planinschek <planinscheksimon@outlook.com>
2020-07-16 20:44:49 +0200

    add bot entry

---
+++ index.js
@@ -0,0 +1,22 @@
+const env = require('dotenv');
+const telegraf = require('telegraf');
+
+// Setup env
+env.config();
+
+// Init telegraf
+const bot = new telegraf(process.env.TELEGRAM_BOT);
+
+// Handle mention
+require('./events')(bot);
+
+// Init commands
+require('./commands')(bot);
+
+// Init scheduler
+require('./scheduler')(bot);
+
+// Start bot
+bot.launch()
+  .then(() => console.log('Bot started!'))
+  .catch(e => console.error(`Starting bot failed: ${e}`));
