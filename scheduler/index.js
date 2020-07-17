5dc779c0cef8efee2b5f8a0e9acd2bda3e3acf3f
Simon Planinschek <planinscheksimon@outlook.com>
2020-07-17 13:27:09 +0200

    add log to scheduler to log errors in docker console

--- scheduler/index.js
+++ scheduler/index.js
@@ -6,6 +6,9 @@ module.exports = (bot) => {
     require('../commands/news')
       .getGeneralNews()
       .then(news => bot.telegram.sendMessage(process.env.CHAT_ID, news))
-      .catch(err =>  bot.telegram.sendMessage(process.env.CHAT_ID, err));
+      .catch(err =>  {
+        console.log(err);
+        bot.telegram.sendMessage(process.env.CHAT_ID, err)
+    });
   });
 }
