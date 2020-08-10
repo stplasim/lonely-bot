adc1b1904866361f2cb09767e9b51d611d2ff528
Simon Planinschek <planinscheksimon@outlook.com>
2020-08-10 17:33:36 +0200

    add typescript version of the main entry point

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { config } from "dotenv";
+import { Telegraf } from "telegraf";
+import "./util/arrayUtility";
+
+// Import components
+import commandHandler from "./commands";
+import mentionHandler from "./events";
+import {logError, logInfo} from "./util/logger";
+import { scheduleNews } from "./scheduler";
+
+// Setup env
+config();
+
+// Init net bot
+// @ts-ignore
+const bot = new Telegraf(process.env.TELEGRAM_BOT);
+
+// Handle mention
+mentionHandler(bot);
+
+// Handle commands
+commandHandler(bot);
+
+// Init scheduler
+scheduleNews(bot);
+
+// Start bot
+bot.launch()
+    .then(() => logInfo(`Bot started! - Version ${process.env.BOT_VERSION}`))
+    .catch(e => logError("Starting bot failed", e));
