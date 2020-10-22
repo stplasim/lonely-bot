59bc7f7d53efdf64f5ac325b0373ba86850a1c41
Simon Planinschek <planinscheksimon@outlook.com>
2020-10-22 19:40:01 +0200

    Added new schedulers

--- src/scheduler/index.ts
+++ src/scheduler/index.ts
@@ -1,14 +1,15 @@
 import { Telegraf } from "telegraf";
 import { schedule } from "node-cron";
-import { generateNews } from "../commands/newsCommand";
 import {logError, logInfo} from "../util/logger";
+import {getMemeFromAPI} from "../commands/memeCommand";
+import {getUselessFactFromAPI} from "../commands/factsCommand";
 
 /**
- * Schedule news
+ * Schedule daily meme
  *
  * @param bot - Main bot instance
  */
-export function scheduleNews(bot: Telegraf<any>) {
+export function scheduleMemes(bot: Telegraf<any>) {
     schedule("0 8 * * *", async function () {
         // Chet id
         const chetId = process.env.CHAT_ID;
@@ -19,19 +20,55 @@ export function scheduleNews(bot: Telegraf<any>) {
         }
 
         try {
-            // Get headlines
-            const newsData = await generateNews(7);
+            // Get daily meme
+            const dailyMeme = await getMemeFromAPI()
 
-            // Send news into group
-            bot.telegram.sendMessage(chetId, newsData, {
-                parse_mode: "Markdown"
-            })
-                .then(() => logInfo(`Send News to group with id${chetId}`))
+            // Send daily meme with text
+            bot.telegram.sendPhoto(chetId, dailyMeme.data.url)
+                .then(() => logInfo(`Send daily meme to group with id ${chetId}`))
+                .catch(err => logError("Something went wrong: ", err));
+
+            bot.telegram.sendMessage(
+                chetId,
+                `Greetings my dudes, here is your daily meme from the ${dailyMeme.data.subreddit} subreddit. `
+            )
+                .then(() => logInfo(`Send daily meme to group with id ${chetId}`))
                 .catch(err => logError("Something went wrong: ", err));
         }
         catch (err) {
             logError("Something went wrong", err);
-            await bot.telegram.sendMessage(chetId, "Sorry but I had trouble getting the news for today.");
+            await bot.telegram.sendMessage(chetId, "Sorry, but I had problems selling your data to the NSA.");
+        }
+    });
+}
+
+/**
+ * Schedule useless fact
+ *
+ * @param bot - Main bot instance
+ */
+export function scheduleFact(bot: Telegraf<any>) {
+    schedule("0 12 * * *", async function () {
+        // Chet id
+        const chetId = process.env.CHAT_ID;
+
+        if(chetId == undefined) {
+            logError("Chet id is not defined");
+            return;
+        }
+
+        try {
+            let message = "Hello, it is once again useless facts time\n"
+            message += "Here is today's\n\n"
+            message += (await getUselessFactFromAPI()).data.text
+
+            bot.telegram.sendMessage(chetId, message)
+                .then(() => logInfo(`Send daily fact to group with id ${chetId}`))
+                .catch(err => logError("Something went wrong: ", err));
+        }
+        catch (err) {
+            logError("Something went wrong", err);
+            await bot.telegram.sendMessage(chetId, "Sorry, but I had problems selling your data to the NSA.");
         }
     });
 }
