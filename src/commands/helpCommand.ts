7668543ca46ecac92fdc5364e6311568893f57bc
Simon Planinschek <planinscheksimon@outlook.com>
2020-10-22 19:41:06 +0200

    Add new useless fact command to help

--- src/commands/helpCommand.ts
+++ src/commands/helpCommand.ts
@@ -10,10 +10,11 @@ export default function (ctx: Context): void {
     ctx.replyWithMarkdown(
         'Hi, my dude, looks like you need some help. ' +
         'Here you have a list of all the commands I understand. \n\n' +
-        '*/help* - Get some help \n\n' +
+        '*/help* - Stop It, Get Some Help \n\n' +
         '*/hello* - Does nobody greet you? No problem, I\'ll do it.\n\n' +
         '*/fact* - Get information that will change your life \n\n' +
-        '*/news* - Here you get some real news.' +
+        '*/useless* - Get information that will change your life, the earth and the whole universe \n\n' +
+        '*/news* - Here you get some real news. \n\n' +
         '*/joke* - Want to hear a bad joke? Then you\'re in the right place.\n\n' +
         '*/meme* - You really need a meme. Here you get one.\n\n' +
         '*/weather* - Get the current weather situation\n\n' +
