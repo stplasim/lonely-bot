47ed0c884dc0e48a88089167f528e67f70f60185
Simon Planinschek <planinscheksimon@outlook.com>
2020-10-22 19:40:52 +0200

    Split fact command into api and command handler

--- src/commands/factsCommand.ts
+++ src/commands/factsCommand.ts
@@ -1,13 +1,21 @@
 import { Context } from "telegraf";
 import { loadFileFromPath } from "../util/fileIO";
 import { logInfo, logError } from "../util/logger";
+import axios, { AxiosResponse } from "axios";
+
+type UselessFactData = {
+    id: string
+    text: string
+    source: string
+    language: string
+}
 
 /**
  * Get random fact from fact file
  *
  * @param ctx - Bot context object
  */
-export default async function(ctx: Context): Promise<void> {
+async function factCommand(ctx: Context): Promise<void> {
     try {
         const data = loadFileFromPath(__dirname, "..", "..", "data", "useless.json");
 
@@ -28,3 +36,35 @@ export default async function(ctx: Context): Promise<void> {
         await ctx.reply("It looks like something didn't quite go as planned. Just try it again my dude.");
     }
 }
+
+/**
+ * Get useless fact from api
+ */
+async function getUselessFactFromAPI(): Promise<AxiosResponse<UselessFactData>> {
+    return await axios.get("https://uselessfacts.jsph.pl/random.json", {
+        headers: {
+            Accept: "application/json"
+        }
+    });
+}
+
+/**
+ * Useless fact command
+ */
+async function uselessFactCommand(ctx: Context): Promise<void> {
+    try {
+
+        const fact = await getUselessFactFromAPI()
+
+        ctx.reply(fact.data.text)
+            .then(() => logInfo(`Send useless fact to user ${ctx.from?.username}`))
+            .catch(err => logError("Something went wrong", err));
+    }
+    catch (err) {
+        logError("Something went wrong", err);
+        await ctx.reply("It looks like something didn't quite go as planned. Just try it again my dude.");
+    }
+}
+
+export { uselessFactCommand, factCommand, getUselessFactFromAPI }
+export type { UselessFactData }
